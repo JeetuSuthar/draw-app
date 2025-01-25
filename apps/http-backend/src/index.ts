6ad5116706c72e8286073f2a10a3c233ac48bfb4
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-01-25 16:15:47 +0530

    Backend MonoRepo Setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,37 @@
 import express from "express";
-const app=express();
-app.listen(3000)
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from '@repo/backend-common/config'
+import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema , CreateRoomSchema} from "@repo/common/types"
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+    const data=CreateUserSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message:"Incorrect credentials"
+        })
+        return;
+    }
+    // db call
+    res.json({
+    userId: "123"
+    })
+})
+app.post("/signin", (req, res) => {
+    
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+    res.json({
+        token
+    })
+})
+app.post("/room", middleware, (req, res) => {
+    // db call
+    res.json({
+        roomId: 123
+    })
+})
