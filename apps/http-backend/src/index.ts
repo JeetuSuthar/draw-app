11a7affe7c4442b81dc6ef214112c7bb8500d0d5
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-02-01 23:23:29 +0530

    Prisma , Signup Error Solved

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from '@repo/backend-common/config'
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
-import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
-import { prismaClient } from "@repo/db/client"
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+
 app.post("/signup", async (req, res) => {
 
     const parsedData = CreateUserSchema.safeParse(req.body);
+   // console.log(parsedData)
     if (!parsedData.success) {
         console.log(parsedData.error);
         res.json({
@@ -17,23 +22,37 @@ app.post("/signup", async (req, res) => {
         return;
     }
     try {
-        const user = await prismaClient.user.create({
-            data: {
-                email: parsedData.data?.username,
-                // TODO: Hash the pw
-                password: parsedData.data.password,
-                name: parsedData.data.name
-            }
-        })
+        console.log("In try")
+        console.log(parsedData.success);
+       
+            const user = await prismaClient.user.create({
+                data: {
+                    email: parsedData.data?.username,
+                    // TODO: Hash the pw
+                    password: parsedData.data.password,
+                    name: parsedData.data.name
+                }
+                
+                
+            })
+      
+        
+        
+        
+      
         res.json({
             userId: user.id
         })
+        
+        
     } catch(e) {
+        
         res.status(411).json({
             message: "User already exists with this username"
         })
     }
 })
+
 app.post("/signin", async(req, res) => {
 
     const parsedData=SigninSchema.safeParse(req.body);
@@ -94,3 +113,21 @@ app.post("/room", middleware, async (req, res) => {
         })
     }
 })
+
+app.post('/chats/:roomId',async(req,res)=>{
+    const roomId=Number(req.params.roomId);
+    const messages=await prismaClient.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50
+    })
+    res.json({
+        messages
+    })
+})
+
+app.listen(3001);
