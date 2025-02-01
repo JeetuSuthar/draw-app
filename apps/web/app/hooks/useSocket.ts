56343dd6c9718dfbfc118723f9d985f2f8f2d634
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-02-02 01:17:57 +0530

    frontend Setup

--- apps/web/app/hooks/useSocket.ts
+++ apps/web/app/hooks/useSocket.ts
@@ -6,7 +6,7 @@ export function useSocket() {
     const [socket, setSocket] = useState<WebSocket>();
 
     useEffect(() => {
-        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxNDMzOTdjMy01OTNkLTQwMjctYjExNC0yOTAyNGJhYjAyMTgiLCJpYXQiOjE3MzY2OTczMzB9.BxDMP3FqBsM6TrZcAGYFRA2FlmazFwQJ78mOHskatiM`);
+        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkY2M0YWM4Yi1jOWNhLTRlN2MtYTZlZi0yZDFkMjFkYjMwOGUiLCJpYXQiOjE3Mzg0MTI4Nzd9.WL2FUqGKcsm6Lh5R-BZqqGM5_LSLjIsGqX5Oxy2xoBA`);
         ws.onopen = () => {
             setLoading(false);
             setSocket(ws);
