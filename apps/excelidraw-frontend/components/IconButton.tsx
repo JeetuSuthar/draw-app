745d89937a31a06ab259faed902dbf6078fc9590
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-02-03 23:12:34 +0530

    Initisl(Some err)

--- apps/excelidraw-frontend/components/IconButton.tsx
+++ apps/excelidraw-frontend/components/IconButton.tsx
@@ -1,13 +1,30 @@
 import { ReactNode } from "react";
 
 export function IconButton({
-    icon, onClick, activated
+    icon,
+    onClick,
+    activated,
+    className,
+    title,
 }: {
-    icon: ReactNode,
-    onClick: () => void,
-    activated: boolean
+    icon: ReactNode;
+    onClick: () => void;
+    activated: boolean;
+    className?: string;
+    title?: string;
 }) {
-    return <div className={`m-2 pointer rounded-full border p-2 bg-black hover:bg-gray ${activated ? "text-red-400" : "text-white"}`} onClick={onClick}>
-        {icon}
-    </div>
+    return (
+        <div
+            // style={{
+            //     fontSize: "1.5rem",
+            // }}
+            className={`cursor-pointer rounded-sm p-1.5 hover:bg-gray ${className} ${
+                activated ? "text-indigo-100 bg-indigo-500/60" : "text-zinc-400"
+            }`}
+            onClick={onClick}
+            title={title}
+        >
+            {icon}
+        </div>
+    );
 }
