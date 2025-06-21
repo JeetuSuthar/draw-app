882c2027ed819ea46c16f9abf4318cdc6445c7d7
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-06-21 20:38:06 +0530

    ok done

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,6 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '../../packages/db/.env' });
 import { PrismaClient } from "@prisma/client";
+
 export const prismaClient = new PrismaClient();
