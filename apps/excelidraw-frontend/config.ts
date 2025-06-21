d54c37b0d6b4f86a1a1b7e74fc461718bd2864d3
jeetu-crafteak <jeetusuthar2315@gmail.com>
2025-06-22 00:13:56 +0530

    env

--- apps/excelidraw-frontend/config.ts
+++ apps/excelidraw-frontend/config.ts
@@ -1,3 +1,6 @@
 
-export const HTTP_BACKEND = "http://localhost:3001"
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_BACKEND = "https://draw-app-backend-bt3f.onrender.com"
+export const WS_URL = "wss://draw-app-ws-bis6.onrender.com";
+
+
+
